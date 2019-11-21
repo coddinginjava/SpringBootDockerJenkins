86f42373832776f49520f9f2872f722ce6d59586
Codding in java <coddinginjava@gmail.com>
2019-11-21 08:45:39 +0530

    first commit

---
+++ src/main/java/com/sai/service/SpringBootDockerJenkins/SpringBootDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.sai.service.SpringBootDockerJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/service/SpringBootDockerJenkins/SpringBootDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.service.SpringBootDockerJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
